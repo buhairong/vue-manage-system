6d1ee2eda54b5f28a695f646b06dba790b2ee88a
buhairong <22858111@qq.com>
2019-11-20 17:47:38 +0800

    15

---
+++ src/components/CommonAside.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-menu default-active="1" class="el-menu-vertical-demo" background-color="#33aef0" text-color="#fff" active-text-color="#ffd04b">
+    <el-menu-item :index="item.path" v-for="item in noChildren" :key="item.path">
+      <i :class="`el-icon-${item.icon}`"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+    <el-submenu :index="item.path" v-for="(item, index) in hasChildren" :key="index">
+      <template slot="title">
+        <i :class="`el-icon-${item.icon}`"></i>
+        <span>{{ item.label }}</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item :index="subItem.path" v-for="subItem in item.children" :key="subItem.path">{{ subItem.label }}</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  computed: {
+    noChildren() {
+      return this.asideMenu.filter(item => !item.children)
+    },
+    hasChildren() {
+      return this.asideMenu.filter(item => item.children)
+    }
+  },
+
+  data() {
+    return {
+      asideMenu: [
+        {
+          path: '/',
+          label: '首页',
+          icon: 'location'
+        },
+        {
+          path: '/video',
+          label: '视频管理',
+          icon: 'video-play'
+        },
+        {
+          path: '/user',
+          label: '用户管理',
+          icon: 'user'
+        },
+        {
+          label: '其他',
+          icon: 'user',
+          children: [
+            {
+              path: '/page1',
+              label: '页面1',
+              icon: 'setting'
+            },
+            {
+              path: '/page2',
+              label: '页面2',
+              icon: 'setting'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-menu {
+  height: 100%;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-container class="wrap">
+    <el-aside width="200px"><common-aside></common-aside></el-aside>
+    <el-container>
+      <el-header><common-header></common-header></el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import CommonHeader from '../components/CommonHeader.vue'
+import CommonAside from '../components/CommonAside.vue'
+
+export default {
+  components: {
+    CommonHeader,
+    CommonAside
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  height: 100%;
+}
+</style>
