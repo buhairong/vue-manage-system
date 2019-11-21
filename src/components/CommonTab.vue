78c58e057bb4a804afbb85f842f81c27e3a3c7f7
buhairong <22858111@qq.com>
2019-11-21 23:02:54 +0800

    18

---
+++ src/components/CommonTab.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tabs">
+    <el-tag :key="tag.name" size="small" v-for="tag in tags" :closable="tag.name !== 'home'" :disable-transitions="false" @close="handleClose(tag)">
+      {{ tag.label }}
+    </el-tag>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+  data() {
+    return {
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      inputVisible: false,
+      inputValue: ''
+    }
+  },
+  computed: {
+    ...mapState({
+      tags: state => state.tab.tabsList
+    })
+  },
+  methods: {
+    ...mapMutations({
+      close: 'closeTab'
+    }),
+    handleClose(tag) {
+      this.close(tag)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tabs {
+  padding: 20px;
+  .el-tag {
+    margin-right: 15px;
+  }
+}
+</style>
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,15 +1,31 @@
 export default {
   state: {
     menu: [],
-    currentMenu: null
+    currentMenu: null,
+    tabsList: [
+      {
+        name: 'home',
+        path: '/',
+        label: '首页',
+        icon: 'location'
+      }
+    ]
   },
   mutations: {
     selectMenu(state, val) {
       if (val.name !== 'home') {
         state.currentMenu = val
+        let result = state.tabsList.findIndex(item => item.name === val.name)
+        if (result === -1) {
+          state.tabsList.push(val)
+        }
       } else {
         state.currentMenu = null
       }
+    },
+    closeTab(state, val) {
+      let result = state.tabsList.findIndex(item => item.name === val.name)
+      state.tabsList.splice(result, 1)
     }
   },
   actions: {}
